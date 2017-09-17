d249d5917ff398778d821d5c81be60569fac31e2
binLearning <binlearning@163.com>
2017-09-17 13:17:03 +0800

    Create resnext_50_deploy.py

---
+++ ResNeXt/resnext_50_deploy.py
@@ -0,0 +1,137 @@
+import os
+import numpy as np
+
+import caffe
+from caffe import layers as L, params as P
+from caffe import to_proto
+
+# first block
+# Convolution - BatchNorm - Scale - ReLU
+def _block_first(net, bottom, nout=64, pad=3, ks=7, stride=2):
+  net.conv1 = L.Convolution(bottom,
+                            num_output = nout, pad = pad,
+                            kernel_size = ks, stride = stride,
+                            bias_term = False)
+  net.bn_conv1 = L.BatchNorm(net.conv1, in_place = True)
+  net.scale_conv1 = L.Scale(net.bn_conv1, bias_term = True, in_place = True)
+  net.conv1_relu = L.ReLU(net.scale_conv1, in_place = True)
+
+  return net.conv1_relu
+
+
+# 3(layer) in 1(block)
+# Convolution - BatchNorm - Scale
+def _block_3in1(major, minor, net, bottom, nout, pad, ks, stride):
+  branch_flag = '{}_branch{}'.format(major, minor)
+  conv_layer  = 'res{}'.format(branch_flag)
+  bn_layer    = 'bn{}'.format(branch_flag)
+  scale_layer = 'scale{}'.format(branch_flag)
+
+  net[conv_layer]  = L.Convolution(bottom,
+                                   num_output = nout, pad = pad,
+                                   kernel_size = ks, stride = stride,
+                                   bias_term = False)
+  net[bn_layer]    = L.BatchNorm(net[conv_layer], in_place = True)
+  net[scale_layer] = L.Scale(net[bn_layer], bias_term = True, in_place = True)
+
+  return net[scale_layer]
+
+
+# 4(layer) in 1(block)
+# Convolution - BatchNorm - Scale - ReLU
+def _block_4in1(major, minor, net, bottom, nout, pad, ks, stride):
+  branch_flag = '{}_branch{}'.format(major, minor)
+  conv_layer  = 'res{}'.format(branch_flag)
+  bn_layer    = 'bn{}'.format(branch_flag)
+  scale_layer = 'scale{}'.format(branch_flag)
+  relu_layer  = 'res{}_relu'.format(branch_flag)
+  
+  if ks == 3: # bottleneck layer, grouped convolutions
+    net[conv_layer]  = L.Convolution(bottom,
+                                     num_output = nout, pad = pad,
+                                     kernel_size = ks, stride = stride,
+                                     bias_term = False,
+                                     group = 32) # Cardinality
+  else: 
+    net[conv_layer]  = L.Convolution(bottom,
+                                     num_output = nout, pad = pad,
+                                     kernel_size = ks, stride = stride,
+                                     bias_term = False)
+  net[bn_layer]    = L.BatchNorm(net[conv_layer], in_place = True)
+  net[scale_layer] = L.Scale(net[bn_layer], bias_term = True, in_place = True)
+  net[relu_layer]  = L.ReLU(net[scale_layer], in_place = True)
+
+  return net[relu_layer]
+
+
+# branch
+#              [3in1] \  
+#                      | - branch
+# 4in1 - 4in1 - 3in1  /
+def _branch(major, net, bottom, nout, has_branch1=False, is_branch_2a=False):
+  eltwise_layer = 'res{}'.format(major)
+  relu_layer    = 'res{}_relu'.format(major)
+
+  stride = 1
+  if has_branch1 and not is_branch_2a:
+    stride = 2
+
+  branch2_2a = _block_4in1(major, '2a', net, bottom,     nout,   0, 1, 1)
+  branch2_2b = _block_4in1(major, '2b', net, branch2_2a, nout,   1, 3, stride)
+  branch2_2c = _block_3in1(major, '2c', net, branch2_2b, nout*2, 0, 1, 1)
+
+  if has_branch1:
+    branch1 = _block_3in1(major, '1',  net, bottom, nout*2, 0, 1, stride)
+    net[eltwise_layer] = L.Eltwise(branch1, branch2_2c)
+  else:
+    net[eltwise_layer] = L.Eltwise(bottom, branch2_2c)
+
+  net[relu_layer] = L.ReLU(net[eltwise_layer], in_place = True)
+
+  return net[relu_layer]
+
+
+def construc_net():
+  net = caffe.NetSpec()
+
+  net.data  = L.Input(input_param = dict(shape = dict(dim = [1,3,224,224])))
+
+  block1    = _block_first(net, net.data)
+
+  net.pool1 = L.Pooling(block1, pool = P.Pooling.MAX, kernel_size = 3, stride = 2)
+
+  branch_2a = _branch('2a', net, net.pool1, 128, has_branch1 = True, is_branch_2a = True)
+  branch_2b = _branch('2b', net, branch_2a, 128)
+  branch_2c = _branch('2c', net, branch_2b, 128)
+
+  branch_3a = _branch('3a', net, branch_2c, 256, has_branch1 = True)
+  branch_3b = _branch('3b', net, branch_3a, 256)
+  branch_3c = _branch('3c', net, branch_3b, 256)
+  branch_3d = _branch('3d', net, branch_3c, 256)
+
+  branch_4a = _branch('4a', net, branch_3d, 512, has_branch1 = True)
+  branch_4b = _branch('4b', net, branch_4a, 512)
+  branch_4c = _branch('4c', net, branch_4b, 512)
+  branch_4d = _branch('4d', net, branch_4c, 512)
+  branch_4e = _branch('4e', net, branch_4d, 512)
+  branch_4f = _branch('4f', net, branch_4e, 512)
+
+  branch_5a = _branch('5a', net, branch_4f, 1024, has_branch1 = True)
+  branch_5b = _branch('5b', net, branch_5a, 1024)
+  branch_5c = _branch('5c', net, branch_5b, 1024)
+
+  net.pool5 = L.Pooling(branch_5c, pool = P.Pooling.AVE, kernel_size = 7, stride = 1)
+  
+  net.fc6 = L.InnerProduct(net.pool5, num_output = 1000)
+  net.prob = L.Softmax(net.fc6)
+
+  return net.to_proto()
+
+
+def main():
+  with open('resnext_50_deploy.prototxt', 'w') as f:
+    f.write('name: "ResNeXt-50_deploy"\n')
+    f.write(str(construc_net()))
+
+if __name__ == '__main__':
+  main()
